bcbe901add04fa77436c0240382678d9c9a26441
Abhishek1106kr <chauhanabhishekkr@gmail.com>
2026-07-09 00:34:34 +0530

    feat:adding port

--- server/server.ts
+++ server/server.ts
@@ -194,7 +194,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ||10000;
 server.listen(PORT, () => {
   console.log(`DuoChat Server is running on http://localhost:${PORT}`);
 });
